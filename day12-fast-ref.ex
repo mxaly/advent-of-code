dfde28d04b7fad92535d4a38a53dab54e0918fe2
Rafał Gawlik <rafal.maly@gmail.com>
2019-12-20 10:03:11 +0100

    day12 p2

---
+++ day12-fast-ref.ex
@@ -0,0 +1,89 @@
+defmodule Scanner do
+  import IEx
+
+  def vel_for_point(x, y, z, points) do
+    points |> Enum.reduce({0,0,0}, fn collision_point, {xa, ya, za} ->
+      {{cx, cy, cz}, _} = collision_point
+
+      {
+        cond do
+          x < cx -> xa + 1
+          x > cx -> xa - 1
+          x == cx -> xa
+        end,
+        cond do
+          y < cy -> ya + 1
+          y > cy -> ya - 1
+          y == cy -> ya
+        end,
+        cond do
+          z < cz -> za + 1
+          z > cz -> za - 1
+          z == cz -> za
+        end
+      }
+    end)
+  end
+
+  def calc_point(point, points) do
+    {{x, y, z}, {vx, vy, vz}} = point
+    {vpx, vpy, vpz} = vel_for_point(x, y, z, points)
+
+    {
+      {
+        vx + vpx + x,
+        vy + vpy + y,
+        vz + vpz + z,
+      },
+      {
+        vx + vpx,
+        vy + vpy,
+        vz + vpz
+      }
+    }
+  end
+
+  # def step(points, _, 2) do points end
+  def step(points, points, itrs) do itrs end
+  def step(points) do
+    moved = points |> Enum.map(fn(point) -> calc_point(point, points) end)
+
+    step(moved, points, 1)
+  end
+
+  def step(points, start_points, i) do
+    moved = points |> Enum.map(fn(point) -> calc_point(point, points) end)
+    step(moved, start_points, i + 1)
+  end
+end
+
+
+input = [
+  {{15, -2, -6 }, {0, 0, 0}},
+  {{-5, -4, -11}, {0, 0, 0}},
+  {{ 0, -6,  0 }, {0, 0, 0}},
+  {{ 5,  9,  6 }, {0, 0, 0}}
+]
+
+i2 = [
+  {{-1, 0, 2}, {0, 0, 0}},
+  {{2, -10, -7}, {0, 0, 0}},
+  {{4, -8, 8}, {0, 0, 0}},
+  {{3, 5, -1}, {0, 0, 0}}
+]
+
+defmodule Benchmark do
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
+end
+
+Benchmark.measure(fn ->
+  Enum.map(0..100, fn _x -> Scanner.step(i2) end)
+end)
+
+
+
--- day12.ex
+++ day12.ex
@@ -1,3 +1,10 @@
+defmodule RC do
+  def gcd(a,0), do: abs(a)
+  def gcd(a,b), do: gcd(b, rem(a,b))
+
+  def lcm(a,b), do: div(abs(a*b), gcd(a,b))
+end
+
 defmodule Scanner do
   import IEx
 
@@ -24,14 +31,41 @@ defmodule Scanner do
     end)
   end
 
-  def step(points, 0) do points end
-  def step(points, rem) do
+  def step(points,  0) do points end
+  def step(points, steps) do
+    moved = points
+      |> add_vel(:x)
+      |> add_vel(:y)
+      |> add_vel(:z)
+    step(moved, steps)
+  end
+
+  def step(points, steps) do
     moved = points
     |> add_vel(:x)
     |> add_vel(:y)
     |> add_vel(:z)
 
-    step(moved, rem-1)
+    step(moved, steps - 1)
+  end
+
+  def axis_step(points, axis) do
+    axis_step(add_vel(points, axis), points, axis, 1)
+  end
+
+  def axis_step(points, points, _axis, it) do
+    it
+  end
+
+  def axis_step(points, start_points, axis, it) do
+    axis_step(add_vel(points, axis), start_points, axis, it + 1)
+  end
+
+  def find_cycle_length(input) do
+    sx = Scanner.axis_step(input, :x)
+    sy = Scanner.axis_step(input, :y)
+    sz = Scanner.axis_step(input, :z)
+    sx |> RC.lcm(sy) |> RC.lcm(sz)
   end
 
   def point_energy(point) do
@@ -51,28 +85,27 @@ end
 
 
 input = [
-%{x: 15, y: -2, z: -6},
-%{x: -5, y: -4, z: -11},
-%{x: 0, y: -6, z: 0},
-%{x: 5, y: 9, z: 6}
+  %{x: 15, y: -2, z: -6, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: -5, y: -4, z: -11, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 0, y: -6, z: 0, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 5, y: 9, z: 6, vel_x: 0, vel_y: 0, vel_z: 0}
 ]
 
 i2 = [
-  %{x: -1, y: 0, z: 2},
-  %{x: 2, y: -10, z: -7},
-  %{x: 4, y: -8, z: 8},
-  %{x: 3, y: 5, z: -1}
+  %{x: -1, y: 0, z: 2, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 2, y: -10, z: -7, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 4, y: -8, z: 8, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 3, y: 5, z: -1, vel_x: 0, vel_y: 0, vel_z: 0}
 ]
 
 i3 = [
-  %{x: -8, y: -10, z: 0},
-  %{x: 5, y: 5, z: 10},
-  %{x: 2, y: -7, z: 3},
-  %{x: 9, y: -8, z: -3}
+  %{x: -8, y: -10, z: 0, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 5, y: 5, z: 10, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 2, y: -7, z: 3, vel_x: 0, vel_y: 0, vel_z: 0},
+  %{x: 9, y: -8, z: -3, vel_x: 0, vel_y: 0, vel_z: 0}
 ]
 
- Scanner.energy_after(input, 1000)
-# Scanner.vel_for_point(%{x: -1, y: 0, z: 2}, i2, :x)
+
 
 
 
