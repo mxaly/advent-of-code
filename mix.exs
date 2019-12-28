459620b2c1145cbab55a7c66edcc1ea5bb9dea7c
Rafał Gawlik <rafal.maly@gmail.com>
2019-12-28 15:47:13 +0100

    use new elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Advent.MixProject do
     [
       app: :advent,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_paths: ["."],
