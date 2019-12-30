7dcc7c78bf3ffd721dd0081dc8b7603ba2d1a8c4
Rafał Gawlik <rafal.maly@gmail.com>
2019-12-31 00:07:47 +0100

    Extract RC

--- lib/day_12/day12.ex
+++ lib/day_12/day12.ex
@@ -1,12 +1,3 @@
-defmodule RC do
-  @moduledoc false
-
-  def gcd(a, 0), do: abs(a)
-  def gcd(a, b), do: gcd(b, rem(a, b))
-
-  def lcm(a, b), do: div(abs(a * b), gcd(a, b))
-end
-
 defmodule Scanner do
   @moduledoc false
 
